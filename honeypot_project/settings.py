29b60614e4708beb903f57ff6fc640209e9c1236
Jaslyn10 <21a18.jaslyn@sjec.ac.in>
2024-11-27 13:30:44 +0530

    testing

--- honeypot_project/settings.py
+++ honeypot_project/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-ybz4hw!+dt^$+a&i%b+9p_18rjm=s)@j($&&_kbnf)a&@+4!h$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
